6dbd079686f9fad65552aa34d7170c697470bf37
capeza <capeza007@gmail.com>
2024-08-02 14:02:57 +0300

    todo done

--- src/components/app/lists/PatientsList/predicates/stringPredicate.ts
+++
@@ -1,9 +0,0 @@
-import { Filter } from '@devexpress/dx-react-grid';
-
-const stringPredicate = (value: string, filter: Filter): boolean => {
-    return filter.value.find((data: string) => data.toLocaleLowerCase() === value.toLocaleLowerCase())
-        ? true
-        : false;
-};
-
-export default stringPredicate;
--- src/components/app/lists/PatientsList/providers/providerColumns.ts
+++
@@ -1,4 +0,0 @@
-import {defaultListExtensions} from '../defaults';
-import { Fields } from '../../../../../states/common/classes/Patient';
-
-export const patientColumns: Fields[] = defaultListExtensions.columns;
